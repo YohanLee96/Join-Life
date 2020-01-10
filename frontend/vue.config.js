cd6902aa33a1df0ac46a5c4d1b09fd8a51c5903d
Yohan Lee <swj03095@naver.com>
2020-01-10 11:37:56 +0900

    node,vue 연동버그 수정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,14 +1,16 @@
+var path = require("path") 
+
 module.exports = { 
-    devServer: { 
-      proxy: { 
-        '/api': { 
-          target: 'http://localhost:3000/api',
-          changeOrigin: true, 
-          pathRewrite: { 
-            '^/api': ''
+  outputDir : path.resolve(__dirname, "../backend/public/"), 
+  devServer: { 
+        proxy: { // proxyTable 설정 
+          '/api': { 
+            target: 'http://localhost:3000/api', 
+            changeOrigin: true , 
+            pathRewrite:{ "^/api" : '' 
           } 
         } 
       } 
-    },
-    outputDir: '../backend/public',
-}
+    } 
+  }
+
