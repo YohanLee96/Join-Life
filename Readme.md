326b4afa8fdf0410cc127d3a4dc07720f23692ec
Yohan Lee <swj03095@naver.com>
2020-01-10 14:44:29 +0900

    시퀄라이즈 적용,supervisor 라이브러리 추가

--- Readme.md
+++ Readme.md
@@ -1,8 +1,25 @@
-## 동아리-스터디 통합 플랫폼
+## Join & Life 플랫폼
 
-동아리,스터디 구인,모집 할 수 있는 플랫폼 입니다.
+동아리,스터디를 보다 손쉽게 찾기 위해 이용할 수 있는 플랫폼
 
 Back-End : Node.js
 
 Front-End  : Vue.js,jQuery,SCSS,HTML
 
+DataBase : Mysql
+
+
+
+- 주요 npm Module
+
+#Node
+1. express  
+2. sequelize mysql2   
+3. sequelize-cli 
+4. Supervisor(사용법 : supervisor [파일명] )
+    
+
+
+#Vue
+1. vue-cli
+2. vue-router
--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var mainRouter = require('./routes/main/mainController');
+var mainRouter = require("./routes/main/mainController");
 
 var app = express();
 
 //vue 연동 라이브러리
 app.use(require("connect-history-api-fallback")());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Sequelize Sync 맞추는 부분
+const models = require("./models");
+models.sequelize
+  .sync()
+  .then(() => {
+    console.log("✓ DB 연결완료.");
+    console.log("  CTRL-C로 서버중지.\n");
+  })
+  .catch(err => {
+    console.error(err);
+    console.log("✗ 커넥션 에러.");
+    process.exit();
+  });
 
-app.use(logger('dev'));
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', mainRouter);
+app.use("/", mainRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,11 +46,12 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
+
 
 module.exports = app;
