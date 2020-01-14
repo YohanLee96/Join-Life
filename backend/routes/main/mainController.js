c9d06f1c55d8d62edd2d923fc9efc4a6f4b24bbd
Yohan Lee <swj03095@naver.com>
2020-01-14 18:06:12 +0900

    nodemon 라이브러리 추가,BackEnd View폴더 제거

--- backend/routes/main/mainController.js
+++ backend/routes/main/mainController.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     let jsonArray = {
-        'test' : 'test',
+        'test' : 'test2',
     };
 
     return jsonArray;
