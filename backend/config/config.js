3a8e6ee2fb461f9045750973f0220843d0b9cf2e
YohanLee <swj03095@naver.com>
2020-01-23 23:08:08 +0900

    env값 충돌 해결

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 const env = process.env;
 
 const development ={
-  username: env.NAME,
-  password: env.PASSWORD,
-  database: env.DATABASE,
-  host: env.HOST,
-  port: env.PORT,
-  dialect: env.DIALECT
+  username: env.DB_NAME,
+  password: env.DB_PASSWORD,
+  database: env.DB_DATABASE,
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  dialect: env.DB_DIALECT
 }
 
 
